b85f95a1da2c9d3db54e6f09c9a727ba18dc2704
Jackson Burns <jc3urns@gmail.com>
2018-01-21 12:59:46 -0500

    did stuff

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,18 @@
+
+
+public class UserInterface {
+
+    //Member variables
+    private int windowHeight;
+    private int windowWidth;
+
+    public UserInterface(){
+        //Default values
+    }
+    public void drawLayout() {
+
+    }
+    public void setupLayout() {
+
+    }
+}
